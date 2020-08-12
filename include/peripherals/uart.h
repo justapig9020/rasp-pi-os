c8361faf96e171aa93d88eff96f3597f8d61d132
= <=>
2020-08-13 02:17:04 +0800

    implement uart init

--- include/peripherals/uart.h
+++ include/peripherals/uart.h
@@ -3,27 +3,35 @@
 
 #include "peripherals.h"
 
-#define UART_BASE (PERIPHERAL_BASE + 0x20100)
-#define UART_DR (UART_BASE)
+#define UART_BASE   (PERIPHERAL_BASE + 0x20100)
+#define UART_DR     (UART_BASE)
 #define UART_RSRECR (UART_BASE + 0x4)
-#define UART_FR (UART_BASE + 0x18)
-#define UART_ILPR (UART_BASE + 0x20)
-#define UART_IBRD (UART_BASE + 0x24)
-#define UART_FBRD (UART_BASE + 0x28)
-#define UART_LCRH (UART_BASE + 0x2c)
-#define UART_CR (UART_BASE + 0x30)
-#define UART_IFLS (UART_BASE + 0x34)
-#define UART_IMSC (UART_BASE + 0x38)
-#define UART_RIS (UART_BASE + 0x3c)
-#define UART_MIS (UART_BASE + 0x40)
-#define UART_ICR (UART_BASE + 0x44)
-#define UART_DMACR (UART_BASE + 0x48)
-#define UART_ITCR (UART_BASE + 0x80)
-#define UART_ITIP (UART_BASE + 0x84)
-#define UART_ITOP (UART_BASE + 0x88)
-#define UART_TDR (UART_BASE + 0x8c)
+#define UART_FR     (UART_BASE + 0x18)
+#define UART_ILPR   (UART_BASE + 0x20)
+#define UART_IBRD   (UART_BASE + 0x24)
+#define UART_FBRD   (UART_BASE + 0x28)
+#define UART_LCRH   (UART_BASE + 0x2c)
+#define UART_CR     (UART_BASE + 0x30)
+#define UART_IFLS   (UART_BASE + 0x34)
+#define UART_IMSC   (UART_BASE + 0x38)
+#define UART_RIS    (UART_BASE + 0x3c)
+#define UART_MIS    (UART_BASE + 0x40)
+#define UART_ICR    (UART_BASE + 0x44)
+#define UART_DMACR  (UART_BASE + 0x48)
+#define UART_ITCR   (UART_BASE + 0x80)
+#define UART_ITIP   (UART_BASE + 0x84)
+#define UART_ITOP   (UART_BASE + 0x88)
+#define UART_TDR    (UART_BASE + 0x8c)
 
-void uart_init(unsigned int);
+
+#define MHZ             *1000000
+#define UART_CLK        48 MHZ
+#define BAUD_RATE       115200
+#define BAUD_RATE_DIV   ((double)UART_CLK / 16 * BAUD_RATE)
+#define UART_DIV_INT    ((unsigned int) BAUD_RATE_DIV)
+#define UART_DIV_BOU    ((double) (BAUD_RATE_DIV - UART_DIV_INT) * 64 + 0.5)
+
+void uart_init(void);
 char uart_getchar();
 int uart_read(char*, int);
 int uart_write(char*, int);
---
+++ src/uart.c
@@ -0,0 +1,35 @@
+#include "peripherals/uart.h"
+
+void uart_init(void)
+{
+    unsigned int selector;
+
+	selector = get32(GPFSEL1);
+	selector &= ~(7<<12);                   // clean gpio14
+	selector |= 4<<12;                      // set alt5 for gpio14
+	selector &= ~(7<<15);                   // clean gpio15
+	selector |= 4<<15;                      // set alt5 for gpio15
+	put32(GPFSEL1,selector);
+
+	put32(GPPUD,0);
+	delay(150);
+	put32(GPPUDCLK0,(1<<14)|(1<<15));
+	delay(150);
+	put32(GPPUDCLK0,0);
+
+    //uart 
+    put32(UART_IBRD, UART_DIV_INT); //set baudrate 115200
+    put32(UART_FBRD, UART_DIV_BOU);
+    put32(UART_LCRH, (1 << 4) | (3 << 5));
+    put32(UART_CR, (1 | (1 << 8) | (1 << 9)));
+      
+
+
+}
+char uart_getchar()
+{
+
+}
+int uart_read(char*, int);
+int uart_write(char*, int);
+
