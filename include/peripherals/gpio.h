5f0797f892bd54dd37cde440e21756d48120a0df
= <=>
2020-08-12 21:51:10 +0800

    gpio.h add

---
+++ include/peripherals/gpio.h
@@ -0,0 +1,12 @@
+#include "peripherals.h"
+
+#ifndef _GPIO_H_
+#define _GPIO_H_
+
+#define GPFSEL1         (PERIPHERAL_BASE+ 0x00200004)
+#define GPSET0          (PERIPHERAL_BASE+ 0x0020001C)
+#define GPCLR0          (PERIPHERAL_BASE+ 0x00200028)
+#define GPPUD           (PERIPHERAL_BASE+ 0x00200094)
+#define GPPUDCLK0       (PERIPHERAL_BASE+ 0x00200098)
+
+#endif
