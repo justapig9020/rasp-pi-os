a6dc0b03b1aaf53785134c3a26716151ec2cfb7f
justapig9020 <justapig9020@gmail.com>
2020-08-23 17:21:31 +0800

    uart hello world done

--- src/main.c
+++ src/main.c
@@ -1,32 +1,11 @@
 #include "peripherals/gpio.h"
+#include "peripherals/uart.h"
 #include <stdint.h>
-//#include "peripherals/uart.h"
 
-// inline void devices_init(void) { uart_init(); }
-
-void init_gpio4() {
-    uint32_t value = *((volatile uint32_t *)GPFSEL0);
-    value &= ~(7 << (3 * 4));
-    value |= 1 << (3 * 4);
-    *((volatile uint32_t *)GPFSEL0) = value;
-}
-
-void set_gpio4() {
-    uint32_t value = *((volatile uint32_t *)GPSET0);
-    value |= 1 << 4;
-    *((volatile uint32_t *)GPSET0) = value;
-}
+static inline void devices_init(void) { uart_init(); }
 
 void init_kernel(void) {
-    // devices_init();
-    // uart_init();
-
-    init_gpio4();
-    set_gpio4();
-    while (1)
-        ;
-
-    /*
+    devices_init();
     uart_write("Hello, world\r\n", sizeof("Hello, world\r\n"));
     while (1) {
         char recv;
@@ -36,5 +15,4 @@ void init_kernel(void) {
             uart_write("\n", 1);
         }
     }
-    */
 }
