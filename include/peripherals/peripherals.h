592fabe521e07a2b1d9f3b736aa82435cd3cae64
= <=>
2020-08-29 15:49:59 +0800

    change perioherals

--- include/peripherals/peripherals.h
+++ include/peripherals/peripherals.h
@@ -1,7 +1,10 @@
 #ifndef _PERIPHERALS_H_
 #define _PERIPHERALS_H_
 
-#define PERIPHERAL_BASE 0x7E000000  // Base address on bus
+#include <stdint.h>
+typedef volatile uint32_t* reg_t;
+typedef volatile uint32_t  regv_t; 
+#define PERIPHERAL_BASE 0x3E000000 
 #define PERIPHERAL_SIZE 0x1000000
 
 #endif
