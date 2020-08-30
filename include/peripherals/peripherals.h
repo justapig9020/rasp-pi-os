b1008c22175b37a438c4045af6d08163da4ba26e
= <=>
2020-08-30 15:19:35 +0800

    change physical address

--- include/peripherals/peripherals.h
+++ include/peripherals/peripherals.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 typedef volatile uint32_t* reg_t;
 typedef volatile uint32_t  regv_t; 
-#define PERIPHERAL_BASE 0x3E000000 
+#define PERIPHERAL_BASE 0x3F000000 
 #define PERIPHERAL_SIZE 0x1000000
 
 #endif
